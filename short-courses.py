18b8c0e5ae53e20569370ca9f315b928b4872e1f
kprussing <kprussing74@gmail.com>
2022-12-30 08:58:41 -0500

    Manually generate the Markdown short course table
    Pandoc injects spurious lines when converting from LaTeX to Markdown
    (because it is not a LaTeX parser).  This makes the table ugly.
    Further, Pandoc breaks the URLs.  To fix this, we just generate the
    Markdown ourselves.

--- short-courses.py
+++ short-courses.py
@@ -59,6 +59,8 @@ if __name__ == "__main__":
                         help="Typeset the lectures with the course")
     parser.add_argument("--no-url", action="store_false", dest="url",
                         help="Disable creating hyperlinks for titles")
+    parser.add_argument("-t", "--to", choices={"latex", "markdown"},
+                        default="latex", help="The output format")
     args = parser.parse_args()
 
     if args.example:
@@ -66,38 +68,86 @@ if __name__ == "__main__":
         sys.exit(0)
 
     courses = yaml.safe_load(args.input)
-    name = lambda c: r"\href{{{URL}}}{{{name}}}" \
-            if args.url and "URL" in c else "{name}"
-    lectures = lambda s: ["",
-            r"\begin{itemize}[nosep]",
-            *[r"\item {0}".format(l) for l in s["lectures"]],
-            r"\end{itemize}"
-            ] if args.lectures and s.get("lectures", []) \
-              else []
+    if args.to == "latex":
+        name = lambda c: r"\href{{{URL}}}{{{name}}}" \
+                if args.url and "URL" in c else "{name}"
+        lectures = lambda s: ["",
+                r"\begin{itemize}[nosep]",
+                *[r"\item {0}".format(l) for l in s["lectures"]],
+                r"\end{itemize}"
+                ] if args.lectures and s.get("lectures", []) \
+                  else []
+    elif args.to == "markdown":
+        name = lambda c: "[{name}]({URL})" if args.url and "URL" in c \
+            else "{name}"
+        lectures = lambda s: ["", *[f"-   {_}" for _ in s["lectures"]]
+                              ] if args.lectures and s.get("lectures", []) \
+                             else []
+    else:
+        raise NotImplementedError
 
     info = [dict(name=name(c).format(**c) + "\n".join(lectures(s)),
                  role=s["role"],
                  date=datetime.date(s["year"], s["month"], s["day"]))
             for c in courses["short-courses"] for s in c["sessions"]]
 
-    args.output.write("\\subsection{{{0}}}\n\n".format(args.title)
-                      if args.title else "")
-    header = "\n".join([r"\begin{{longtable}}"
-                        r"{{p{{0.8in}}p{{\dimexpr\textwidth-\leftskip-1.8in}}p{{1.0in}}}}",
-                        r"{0} & {1} & {2} \\",
-                        r"\cmidrule(lr){{1-1}}"
-                        r"\cmidrule(lr){{2-2}}"
-                        r"\cmidrule(lr){{3-3}}"
-                        ])
-    args.output.write(header.format(r"\textbf{Date}",
-                                    r"\textbf{Course}",
-                                    r"\textbf{Contribution}"))
-    line = ("{date:%Y-%m-%d}" if args.iso
-                              else "{date.day} {date:%b} {date:%Y}") \
-         + " & {name} & {role}"
-    text = [line.format(**c) for c in sorted(info,
-                                             key=lambda c: c["date"],
-                                             reverse=True)]
-    args.output.write("\n" + "\\\\\n".join(text) + "\n")
-    args.output.write(r"\end{longtable}")
 
+    datefmt = "{date:%Y-%m-%d}" if args.iso \
+        else "{date.day} {date:%b} {date:%Y}"
+    if args.to == "latex":
+        args.output.write("\\subsection{{{0}}}\n\n".format(args.title)
+                          if args.title else "")
+        header = "\n".join([r"\begin{{longtable}}"
+                            r"{{p{{0.8in}}p{{\dimexpr\textwidth-\leftskip-1.8in}}p{{1.0in}}}}",
+                            r"{0} & {1} & {2} \\",
+                            r"\cmidrule(lr){{1-1}}"
+                            r"\cmidrule(lr){{2-2}}"
+                            r"\cmidrule(lr){{3-3}}"
+                            ])
+        args.output.write(header.format(r"\textbf{Date}",
+                                        r"\textbf{Course}",
+                                        r"\textbf{Contribution}"))
+        line = datefmt + "& {name} & {role}"
+        text = [line.format(**c) for c in sorted(info,
+                                                 key=lambda c: c["date"],
+                                                 reverse=True)]
+        args.output.write("\n" + "\\\\\n".join(text) + "\n")
+        args.output.write(r"\end{longtable}")
+    elif args.to == "markdown":
+        args.output.write(f"## {args.title}\n\n" if args.title else "")
+        datelen = max(len(datefmt.format(**_)) for _ in info)
+        rolelen = max(len(_["role"]) for _ in info)
+        namelen = max(
+            max(len(line.rstrip()) for line in _["name"].splitlines())
+            for _ in info
+        )
+        print(datelen, rolelen, namelen)
+        line = "| " + " | ".join([f"{{0:{datelen}s}}", f"{{1:{namelen}s}}",
+                           f"{{2:{rolelen}s}}"])  + " |"
+        args.output.write("+:" + "-" * (datelen + 1) +
+                          "+:" + "-" * (namelen + 1) +
+                          "+:" + "-" * (rolelen + 1) +
+                          "+\n" +
+                          line.format("**Date**",
+                                      "**Course**",
+                                      "**Contribution**") + "\n" +
+                          "+" + "-" * (datelen + 2) +
+                          "+" + "-" * (namelen + 2) +
+                          "+" + "-" * (rolelen + 2) +
+                          "+\n"
+                          )
+
+        for item in sorted(info, key=lambda _: _["date"], reverse=True):
+            lines = item["name"].splitlines()
+            args.output.write(line.format(datefmt.format(**item),
+                                          lines[0], item["role"]) +
+                              "\n" + line.format("", "", "") + "\n" +
+                              "\n".join([line.format("", _, "")
+                                         for _ in lines[1:]]) + "\n" +
+                              "+" + "-" * (datelen + 2) +
+                              "+" + "-" * (namelen + 2) +
+                              "+" + "-" * (rolelen + 2) +
+                              "+\n"
+                              )
+    else:
+        raise NotImplementedError
